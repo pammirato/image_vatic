b8f770272049883fceacf97359cb43e440fb02c0
Wei Liu <lwbiosoft@gmail.com>
2015-09-09 14:21:18 -0400

    add resize option to formatframes

--- cli.py
+++ cli.py
@@ -70,6 +70,10 @@ class formatframes(Command):
         parser.add_argument("video")
         parser.add_argument("output")
         parser.add_argument("--extension", default="jpg")
+        parser.add_argument("--width", default=720, type=int)
+        parser.add_argument("--height", default=480, type=int)
+        parser.add_argument("--no-resize",
+            action="store_true", default = False)
         parser.add_argument("--no-cleanup",
             action="store_true", default=False)
         return parser
@@ -91,11 +95,20 @@ class formatframes(Command):
         for frame, file in files:
             path = Video.getframepath(frame, args.output)
             file = os.path.join(args.video, file)
-            try:
-                os.link(file, path)
-            except OSError:
-                os.makedirs(os.path.dirname(path))
-                os.link(file, path)
+            if args.no_resize:
+                try:
+                    os.link(file, path)
+                except OSError:
+                    os.makedirs(os.path.dirname(path))
+                    os.link(file, path)
+            else:
+                image = Image.open(file)
+                image.thumbnail((args.width, args.height), Image.BILINEAR)
+                try:
+                    image.save(path)
+                except IOError:
+                    os.makedirs(os.path.dirname(path))
+                    image.save(path)
         print "Formatted {0} frames".format(len(files))
 
 @handler("Imports a set of video frames")
@@ -198,7 +211,7 @@ class load(LoadCommand):
 
         # create video
         video = Video(slug = args.slug,
-                      location = os.path.realpath(args.location), 
+                      location = os.path.realpath(args.location),
                       width = width,
                       height = height,
                       totalframes = maxframes,
@@ -250,7 +263,7 @@ class load(LoadCommand):
 
         print "Creating segments..."
         # create shots and jobs
-       
+
         if args.for_training:
                 segment = Segment(video = video)
                 if args.for_training_start:
@@ -281,7 +294,7 @@ class load(LoadCommand):
                         stop = min(start + segmentlength + args.overlap + 1,
                                    ustop)
                         segment = Segment(start = start,
-                                          stop = stop, 
+                                          stop = stop,
                                           video = video)
                         job = Job(segment = segment, group = group)
                         session.add(segment)
@@ -416,7 +429,7 @@ class DumpCommand(Command):
         video = video.one()
 
         if args.merge:
-            for boxes, paths in merge.merge(video.segments, 
+            for boxes, paths in merge.merge(video.segments,
                                             threshold = args.merge_threshold):
                 workers = list(set(x.job.workerid for x in paths))
                 tracklet = DumpCommand.Tracklet(paths[0].label.text,
@@ -464,7 +477,7 @@ class visualize(DumpCommand):
 
     def __call__(self, args):
         video, data = self.getdata(args)
-        
+
         # prepend class label
         for track in data:
             for box in track.boxes:
@@ -803,7 +816,7 @@ class dump(DumpCommand):
                     lastframe = box.frame
                 else:
                     output = True
-                    
+
                 if output:
                     file.write("<event>");
                     file.write("<username>anonymous</username>")
@@ -830,7 +843,7 @@ class dump(DumpCommand):
 
         file.write("</annotation>")
         file.write("\n")
-    
+
     def dumppascal(self, folder, video, data, difficultthresh, skip,
                    negdir):
         byframe = {}
@@ -855,7 +868,7 @@ class dump(DumpCommand):
             os.makedirs("{0}/JPEGImages/".format(folder))
         except:
             pass
-        
+
         numdifficult = 0
         numtotal = 0
 
@@ -1061,7 +1074,7 @@ class sample(Command):
                                     label = True) for x in job.paths]
 
                 if args.frames > job.segment.stop - job.segment.start:
-                    frames = range(job.segment.start, job.segment.stop + 1) 
+                    frames = range(job.segment.start, job.segment.stop + 1)
                 else:
                     frames = random.sample(xrange(job.segment.start,
                                                 job.segment.stop + 1),
@@ -1167,7 +1180,7 @@ class listvideos(Command):
                 videos = videos.join(Segment)
                 videos = videos.join(Job)
                 videos = videos.filter(Job.completed == True)
-        
+
         if args.count:
             print videos.count()
         else:
