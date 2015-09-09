6878f930ae87a9f9fb4373cbe50aeea73b322246
Wei Liu <lwbiosoft@gmail.com>
2015-09-09 17:55:53 -0400

    update readme

--- vatic-install.sh
+++ vatic-install.sh
@@ -1,11 +1,12 @@
-sudo apt-get install -y git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server-5.1 mysql-client-5.1 libmysqlclient-dev gfortran 
+sudo apt-get install -y git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server mysql-client libmysqlclient-dev gfortran 
 
-sudo easy_install -U SQLAlchemy wsgilog pil cython mysql-python munkres parsedatetime argparse
+sudo easy_install -U SQLAlchemy wsgilog pil mysql-python munkres parsedatetime argparse
 sudo easy_install -U numpy
+sudo pip install cython==0.20
 
-git clone https://github.com/cvondrick/turkic.git
+git clone https://github.com/weiliu89/turkic.git
 git clone https://github.com/cvondrick/pyvision.git
-git clone https://github.com/cvondrick/vatic.git
+git clone https://github.com/weiliu89/vatic.git
 
 cd turkic
 sudo python setup.py install
